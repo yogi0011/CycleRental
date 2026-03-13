b61fd37fc51ba37823d8cfe5980001745fe02707
yogi0011 <dyogi3525@gmail.com>
2026-03-13 11:50:48 +0530

    fix prisma client import

--- cycletwo/prisma.config.ts
+++ cycletwo/prisma.config.ts
@@ -1,13 +1,21 @@
+// import { defineConfig } from "prisma/config";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   datasource: {
+//     url: process.env.DIRECT_URL!,
+//   },
+//   migrations: {
+//     path: "prisma/migrations",
+//   }
+// });
 import { defineConfig } from "prisma/config";
 import dotenv from "dotenv";
 dotenv.config();
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
-    url: process.env.DIRECT_URL!,
-  },
-  migrations: {
-    path: "prisma/migrations",
-  }
+  datasource: { url: process.env.DIRECT_URL! },
+  migrations: { path: "prisma/migrations" }
 });
--- cycletwo/src/utils/prismaClient.ts
+++ cycletwo/src/utils/prismaClient.ts
@@ -1,11 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!
+// });
+
+// export const prisma = new PrismaClient({ adapter });
+import { PrismaClient } from "../generated/prisma/index.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import dotenv from "dotenv";
-
 dotenv.config();
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!
-});
-
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 export const prisma = new PrismaClient({ adapter });
