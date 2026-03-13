3eb1783c9f6e99ab4b3dd3c9f7f34fa640f36be0
yogi0011 <dyogi3525@gmail.com>
2026-03-13 11:57:53 +0530

    fix prisma import

--- cycletwo/src/utils/prismaClient.ts
+++ cycletwo/src/utils/prismaClient.ts
@@ -1,15 +1,4 @@
-// import { PrismaClient } from "@prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL!
-// });
-
-// export const prisma = new PrismaClient({ adapter });
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import dotenv from "dotenv";
 dotenv.config();
